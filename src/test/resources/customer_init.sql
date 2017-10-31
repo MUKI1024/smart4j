1197ee302d758e90631c2270f2ee66beec06abb7
MUKI <muki_1024@163.com>
2017-10-31 21:33:58 +0800

    smart4j chapter2 finish!

---
+++ src/test/resources/customer_init.sql
@@ -0,0 +1,4 @@
+TRUNCATE customer;
+INSERT INTO customer (name, contact, telephone, email, remark) VALUES ('customer1', 'Jack', '13349562354', 'jack@163.com', null);
+INSERT INTO customer (name, contact, telephone, email, remark) VALUES ('customer2', 'Rose', '13349562355', 'rose@163.com', null);
+INSERT INTO customer (name, contact, telephone, email, remark) VALUES ('customer3', 'Kate', '13349562356', 'kate@163.com', null);
